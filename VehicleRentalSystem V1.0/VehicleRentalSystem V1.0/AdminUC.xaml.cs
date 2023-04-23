7268654af409009646e73a8c85cfada342043a46
Valenteno Lenora <86039155+NightKing-V@users.noreply.github.com>
2023-04-23 15:52:12 +0530

    UI Update
    Bookings
    Vehicles
    Details
    Admin
    
    User Controls are DONE!!

---
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/AdminUC.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VehicleRentalSystem_V1._0
+{
+    /// <summary>
+    /// Interaction logic for AdminUC.xaml
+    /// </summary>
+    public partial class AdminUC : UserControl
+    {
+        public AdminUC()
+        {
+            InitializeComponent();
+        }
+
+        private void VehicleInUseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VehicleAvBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
