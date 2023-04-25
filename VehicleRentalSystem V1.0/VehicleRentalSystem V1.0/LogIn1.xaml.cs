f7d10db3901a42f543cc8a62573ca45d4b5dc2c1
dkpthilakawardhana <115579631+dkpthilakawardhana@users.noreply.github.com>
2023-04-25 20:14:43 +0530

    Forms UI(2)
    Login and Forgot passowrd

---
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/LogIn1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VehicleRentalSystem_V1._0
+{
+    /// <summary>
+    /// Interaction logic for LogIn1.xaml
+    /// </summary>
+    public partial class LogIn1 : Window
+    {
+        public LogIn1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void txtUser_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
