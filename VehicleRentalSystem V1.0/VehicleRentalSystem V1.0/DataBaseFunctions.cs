7c9a6959963b8ca4d443a407b3456272ae530394
dkpthilakawardhana <115579631+dkpthilakawardhana@users.noreply.github.com>
2023-04-29 12:49:28 +0530

    Function Constructor For DataBase
    Encap(Get ,set)

---
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/DataBaseFunctions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalSystem_V1._0
+{
+    internal class DataBaseFunctions
+    {
+        private SqlConnection sqlcon;
+        private SqlCommand sqlcmd;
+        private DataTable dt;
+        private String ConStr;
+        private SqlDataAdapter dataadapter;
+
+        public DataBaseFunctions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\lenovo\Documents\GitHub\CSharp-GroupProject\VehicleRentalSystem V1.0\VehicleRentalSystem V1.0\VehicleRentalDB.mdf"";Integrated Security=True";
+            sqlcon = new SqlConnection(ConStr);
+            sqlcmd = new SqlCommand();
+            sqlcmd.Connection = sqlcon;
+
+        }
+
+        public int setdata(string Query)
+        {
+            int cnt = 0;
+            if (sqlcon.State == ConnectionState.Closed)
+            {
+                sqlcon.Open();
+            }
+            sqlcmd.CommandText = Query;
+            cnt = sqlcmd.ExecuteNonQuery();
+            sqlcon.Close();
+            return cnt;
+        }
+        public DataTable getdata(string Query)
+        {
+            dt = new DataTable();
+            dataadapter = new SqlDataAdapter(Query, ConStr);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataadapter);
+            dataadapter.Fill(dt);
+            return dt;
+        }
+    }
+}
