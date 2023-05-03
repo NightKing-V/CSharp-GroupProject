c1ec8c46e698e9ce83a85913b5fada6e5138f445
dkpthilakawardhana <115579631+dkpthilakawardhana@users.noreply.github.com>
2023-05-03 11:06:34 +0530

    database
    database

--- VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/ManageVehicleFor.xaml.cs
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/ManageVehicleFor.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace VehicleRentalSystem_V1._0
 {
@@ -19,7 +21,9 @@ namespace VehicleRentalSystem_V1._0
     /// Interaction logic for ManageVehicleFor.xaml
     /// </summary>
     public partial class ManageVehicleFor : Window
+
     {
+        DataBaseFunctions con;
         public ManageVehicleFor()
         {
             InitializeComponent();
@@ -56,7 +60,95 @@ namespace VehicleRentalSystem_V1._0
 
         private void btnInsert__Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (txtV_No.Text==""||
+                    txtV_Model.Text == ""||
+                    txtV_ChassisNo.Text == ""||
+                    txtV_Brand.Text == ""||
+                    txtV_No.Text == ""||
+                    CarCheckBox.IsChecked == false||
+                    BikeCheckBox.IsChecked == false||
+                    VanCheckBox.IsChecked == false||
+                    _3wheelCheckBox.IsChecked == false||
+                    BusCheckBox.IsChecked == false||
+                    LorryCheckBox.IsChecked == false||
+                    No_of_Passengers.Text == ""||
+                    Vehicle_Condition.Text == ""||
+                    SelectedImage.Source == null)
+                {
+                    MessageBox.Show("Data Missing");
+                }
 
+                else
+                {
+                    string V_PN = txtV_No.Text; 
+                    string V_CN = txtV_ChassisNo.Text;
+                    //vehicle plate number not included include it
+                    string V_Brand = txtV_Brand.Text;
+                    string V_Model = txtV_Model.Text;
+                    string V_type = "";
+
+                    if (CarCheckBox.IsChecked == true)
+                    {
+                        V_type += "Car, ";
+                    }
+                    if (BikeCheckBox.IsChecked == true)
+                    {
+                        V_type += "Bike, ";
+                    }
+                    if (VanCheckBox.IsChecked == true)
+                    {
+                        V_type += "Van, ";
+                    }
+                    if (_3wheelCheckBox.IsChecked == true)
+                    {
+                        V_type += "3 Wheel, ";
+                    }
+                    if (BusCheckBox.IsChecked == true)
+                    {
+                        V_type += "Bus, ";
+                    }
+                    if (LorryCheckBox.IsChecked == true)
+                    {
+                        V_type += "Lorry, ";
+                    }
+
+                    // Remove the last comma and space from the string
+                    V_type = V_type.TrimEnd(',', ' ');
+
+                    // Use the V_type variable as needed
+                    string V_Passenger= No_of_Passengers.Text;
+                    string V_Condition = Vehicle_Condition.Text;
+
+
+                    if (SelectedImage.Source != null)
+                    {
+                        string imageName = SelectedImage.Source.ToString();
+                        // Use the imageName variable as needed
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please select an image");
+                    }
+
+                    string V_State = V_Condition;
+
+                    string Query1 = "INSERT INTO Vehicle (V_PN,V_CN,V_Brand,V_Model,V_type,V_Passenger,V_Condition,V_ImageFolderPath) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}','{5}','{6}','{7}')";
+                    Query1 = string.Format(Query1,V_PN,V_CN,V_Brand,V_Model,V_type,V_Passenger,V_Condition,SelectedImage );
+                    con.setdata(Query1);
+
+
+
+
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnUpdate_Click(object sender, RoutedEventArgs e)
--- VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/RentalForms.xaml.cs
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/RentalForms.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace VehicleRentalSystem_V1._0
 {
@@ -19,15 +21,62 @@ namespace VehicleRentalSystem_V1._0
     /// </summary>
     public partial class RentalForms : Window
     {
+        DataBaseFunctions con;
         public RentalForms()
         {
             InitializeComponent();
+            con = new DataBaseFunctions();
         }
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if(txtUser.Text=="" || txtUser.Text == "" || txtC_Telephone.Text == ""||txtC_Email.Text== "" ||C_address.Text == ""||V_Chassis.Text == ""||VehicleNumber.Text == ""|| Startdate.Text == ""||Startdate == null||  Enddate.Text == ""|| Enddate == null||Returndate.Text == ""||Returndate ==null)
+                {
+                    MessageBox.Show("Data Missing");
+                }
+                else
+                {
+                    
+                    string C_Name = txtUser.Text;
+                    string C_NIC = txtC_NIC.Text;
+                    string C_Tel = txtC_Telephone.Text;
+                    string C_Address = C_address.Text;
+                    string C_Email = txtC_Email.Text;
+                    string V_chassis = V_Chassis.Text;
+                    string package = VehicleNumber.Text;
+                    DateTime StartDate = Startdate.SelectedDate.Value;
+                    DateTime EndDate = Enddate.SelectedDate.Value;
+                    DateTime ReturnDate = Returndate.SelectedDate.Value;
 
+                    //INSERTING TO CUSTOMER TABLE
+                    string Query1 = "INSERT INTO Customer (C_NAME, C_NIC,C_Tel,C_Address,C_Email) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')";
+                    Query1 = string.Format(Query1, C_Name, C_NIC, C_Tel, C_Address, C_Email);
+                    con.setdata(Query1);
+
+                    //INSERTING TO VEHICLE TABLE
+                    //string Query2 = "INSERT INTO Vehicle (V_CN, V_PN) VALUES ('{0}', '{1}')";
+                    //Query2 = string.Format(Query2, V_chassis,package );
+                    //con.setdata(Query2);
+
+                    //INSEERTING TO VEHICLE RENT TABLE
+                    string Query3 = "INSERT INTO VehicleRent (C_NIC,V_CN, VehicleNumber,StartDate,EndDate,ReturnDate) VALUES ('{0}', '{1}','{2}','{3}', '{4}','{5}')";
+                    Query3 = string.Format(Query3,C_NIC,V_chassis,package,StartDate,EndDate,ReturnDate);
+                    con.setdata(Query3);
+
+
+                    MessageBox.Show("Successfully Added");
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
+        
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
@@ -35,18 +84,15 @@ namespace VehicleRentalSystem_V1._0
             txtC_NIC.Text = "";
             txtC_Telephone.Text = "";
             txtC_Email.Text = "";
-            C_Address.Text="";
+            C_address.Text="";
             V_Chassis.Text = "";
             VehicleNumber.Text = "";
-            StartDate.Text = "";
-            StartDate = null;
-            EndDate.Text = "";
-            EndDate =null;
-            ReturnDate.Text = "";
-            ReturnDate=null;
-
-
-
+            Startdate.Text = "";
+            Startdate = null;
+            Enddate.Text = "";
+            Enddate =null;
+            Returndate.Text = "";
+            Returndate =null;
         }
     }
 }
