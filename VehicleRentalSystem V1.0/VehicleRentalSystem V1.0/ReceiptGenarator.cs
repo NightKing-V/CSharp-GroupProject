2035fa9ec86fd426f77e4758a7b412820686529f
Valenteno Lenora <86039155+NightKing-V@users.noreply.github.com>
2023-06-20 16:06:28 +0530

    Receipt Generator -1

---
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/ReceiptGenarator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRentalSystem_V1._0
+{
+    internal class ReceiptGenarator
+    {
+        public double RentFee()
+        {
+            //
+            return 0;
+        }
+        public double HireFee()
+        {
+            //oilfee = etamilage*oilprice
+            //oilfee+
+            return 0;
+        }
+        public void HireReceipt()
+        {
+            string html = 
+        }
+    }
+}
