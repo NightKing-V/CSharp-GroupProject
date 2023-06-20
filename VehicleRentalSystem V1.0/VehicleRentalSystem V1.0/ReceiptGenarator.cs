49bfcd45230518f98a9bc42a97afd84a84ee4028
Valenteno Lenora <86039155+NightKing-V@users.noreply.github.com>
2023-06-20 16:59:54 +0530

    RentFee Calculation

--- VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/ReceiptGenarator.cs
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/ReceiptGenarator.cs
@@ -8,10 +8,42 @@ namespace VehicleRentalSystem_V1._0
 {
     internal class ReceiptGenarator
     {
-        public double RentFee()
+        private string startdate, enddate, returndate, duration;
+        private double Rental, fee, penaltyfee, totalfee;
+        private int penaltyrate;
+        public void RentFee()
         {
-            //
-            return 0;
+            var startdate = Convert.ToDateTime(this.startdate);
+            var enddate = Convert.ToDateTime(this.enddate);
+            var returndate = Convert.ToDateTime(this.returndate);
+
+            var validdays = (enddate - startdate).TotalDays;
+            var invaliddays = (returndate - enddate).TotalDays;
+
+            if (duration == "1Dy") 
+            {
+                fee = validdays * Rental;
+                penaltyfee = invaliddays * Rental * penaltyrate / 100;
+                totalfee = fee + penaltyfee;
+            }
+            else if (duration == "7Dys") 
+            {
+                fee = validdays / 7 * Rental;
+                penaltyfee = invaliddays / 7 * Rental * penaltyrate / 100;
+                totalfee = fee + penaltyfee;
+            }
+            else if (duration == "30Dys")
+            {
+                fee = validdays / 30 * Rental;
+                penaltyfee = invaliddays / 30 * Rental * penaltyrate / 100;
+                totalfee = fee + penaltyfee;
+            }
+            else if (duration == "12Mnths")
+            {
+                fee = validdays / 365 * Rental;
+                penaltyfee = invaliddays / 365 * Rental * penaltyrate / 100;
+                totalfee = fee + penaltyfee;
+            }
         }
         public double HireFee()
         {
