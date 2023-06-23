8f92fe7ae026da4485fd604935416ab8857d3226
dkpthilakawardhana <115579631+dkpthilakawardhana@users.noreply.github.com>
2023-06-23 22:53:38 +0530

    Database
    Driver,Vehicle

--- VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/ManageDriverForm.xaml.cs
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/ManageDriverForm.xaml.cs
@@ -31,7 +31,34 @@ namespace VehicleRentalSystem_V1._0
 
         private void btnD_Insert__Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                string DriverName = txtD_Name.Text;
+                string DriverNIC = txtD_NIC.Text;
+                string DriverLicense = txtD_lisence.Text;
+                string DriverEmail = txtD_Email.Text;
+                string DriverAdd = txtD_Name1.Text;
+                string DriverPhone = DriversPhone.Text;
+                string DState = DriverState.Text;
+                bool Car = CarCheckBox.IsChecked??false;
+                bool bike = BikeCheckBox.IsChecked ?? false;
+                bool van = VanCheckBox.IsChecked ?? false;
+                bool threewheel = _3wheelCheckBox.IsChecked ?? false;
+                bool bus = BusCheckBox.IsChecked ?? false;
+                bool lorry = LorryCheckBox.IsChecked ?? false;
 
+                DataBaseFunctions dbFunctions = new DataBaseFunctions();
+
+                dbFunctions.setdata("INSERT INTO Rider (R_NIC, R_LN, R_Name, R_Email, R_Tel, R_Address, Car, Bike, Van, ThreeWheel, Bus, Lorry, R_State) VALUES ('" + DriverNIC + "','" + DriverLicense + "','" + DriverName + "','" + DriverEmail + "','" + DriverPhone + "','" + DriverAdd + "','" + Car + "','" + bike + "','" + van + "','" + threewheel + "','" + bus + "','" + lorry + "','" + DState + "')");
+
+
+                MessageBox.Show("Inserted Successfully");
+                this.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void btnD_Delete_Click(object sender, RoutedEventArgs e)
