592eae09873e0f32b56b7907e837ec3f662be8de
Valenteno Lenora <86039155+NightKing-V@users.noreply.github.com>
2023-06-26 08:56:17 +0530

    update

--- VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/DataBaseFunctions.cs
+++ VehicleRentalSystem V1.0/VehicleRentalSystem V1.0/DataBaseFunctions.cs
@@ -25,10 +25,9 @@ namespace VehicleRentalSystem_V1._0
 
             //ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\lenovo\Documents\GitHub\CSharp-GroupProject\VehicleRentalSystem V1.0\VehicleRentalSystem V1.0\VehicleRentalDB.mdf"";Integrated Security=True";
 
-            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Program Files(x86)\Common Files\VehicleRentalSystem V1.0\VehicleRentalDB.mdf;Integrated Security=True";
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Program Files (x86)\Common Files\VehicleRentalSystem V1.0\VehicleRentalDB.mdf;Integrated Security=True";
 
             sqlcon = new SqlConnection(ConStr);
-
         }
 
         public void conopen()
